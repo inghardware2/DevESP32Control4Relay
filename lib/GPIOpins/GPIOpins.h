c70c01e1e35506f1b94b64fc1568ea3903a13d36
AmericoAC <inge.americo@gmail.com>
2018-10-17 23:04:02 -0500

    add

--- lib/GPIOpins/GPIOpins.h
+++ lib/GPIOpins/GPIOpins.h
@@ -6,37 +6,37 @@ using namespace std;
 
 class GPIOPin
 {
-private:
-  int GPIO1 = 1,
-      GPIO2 = 2,
-      GPIO3 = 3,
-      GPIO4 = 4,
-      GPIO5 = 5,
-      GPIO12 = 12,
-      GPIO13 = 13,
-      GPIO14 = 14,
-      GPIO15 = 15,
-      GPIO16 = 16,
-      GPIO17 = 17,
-      GPIO18 = 18,
-      GPIO19 = 19,
-      GPIO21 = 21,
-      GPIO22 = 22,
-      GPIO23 = 23,
-      GPIO25 = 25,
-      GPIO26 = 26,
-      GPIO27 = 27,
-      GPIO32 = 32,
-      GPIO33 = 33,
-      GPIO34 = 34,
-      GPIO35 = 35,
-      GPIO36 = 36,
-      GPIO39 = 39;
+  private:
+    int GPIO1 = 1,
+        GPIO2 = 2,
+        GPIO3 = 3,
+        GPIO4 = 4,
+        GPIO5 = 5,
+        GPIO12 = 12,
+        GPIO13 = 13,
+        GPIO14 = 14,
+        GPIO15 = 15,
+        GPIO16 = 16,
+        GPIO17 = 17,
+        GPIO18 = 18,
+        GPIO19 = 19,
+        GPIO21 = 21,
+        GPIO22 = 22,
+        GPIO23 = 23,
+        GPIO25 = 25,
+        GPIO26 = 26,
+        GPIO27 = 27,
+        GPIO32 = 32,
+        GPIO33 = 33,
+        GPIO34 = 34,
+        GPIO35 = 35,
+        GPIO36 = 36,
+        GPIO39 = 39;
 
-public:
-  void selectGPIO(int selectedGPIO, int IO, int state);
-  void stateHIGH(int Hpin);
-  void stateLOW(int Lpin);
+  public:
+    void selectGPIO(int selectedGPIO, int IO, int state);
+    void stateHIGH(int Hpin);
+    void stateLOW(int Lpin);
 };
 //
 void GPIOPin::selectGPIO(int selectedGPIO, int IO, int state)
